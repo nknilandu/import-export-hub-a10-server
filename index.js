d983b0ccd31e8fa2b33d3d1fd457506de1077114
nknilandu <nknilandu@gmail.com>
2025-11-13 14:55:02 +0600

    get and post product and get latest 6 product from mongoDB

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3031;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   "mongodb+srv://importExportHubAdmin:wGQYgWvG5JPC7L1I@cluster.sofxk3k.mongodb.net/?appName=Cluster";
@@ -24,7 +23,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-        // Send a ping to confirm a successful connection
+
+    const appDB = client.db("ImportExportHub");
+    const appCollection = appDB.collection("products");
+    const importCollection = appDB.collection("imports");
+
+    // post a product
+    app.post("/add-product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await appCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // get all product
+    app.get("/all-products", async (req, res) => {
+      const result = await appCollection.find().toArray();
+      res.send(result);
+    });
+    // get latest 6 product
+    app.get("/latest-products", async (req, res) => {
+      const result = await appCollection
+        .find()
+        .sort({ dateAdded: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -36,11 +61,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
   res.send("Welcome to Import Export Hub");
 });
-
 
 app.listen(port, () => {
   console.log(`This app listening on port ${port}`);
